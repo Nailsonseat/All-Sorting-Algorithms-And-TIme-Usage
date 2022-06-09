e90226952cef91ddeebc61529c152fab7d920496
Aadarsh Verma <aadarshv3@gmail.com>
2022-06-09 23:20:05 +0530

    Added all the Major sorting algorithms and final touches

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <iomanip>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 using namespace std::chrono;
+
+void merge(int* arr, int lb, int mid, int ub) {
+	int i(lb), j(mid + 1), k(lb), * ret(new int[ub - lb + 1]);
+	while (i <= mid && j <= ub) {
+		if (arr[i] <= arr[j])ret[k++ - lb] = arr[i++];
+		else ret[k++ - lb] = arr[j++];
+	}
+	if (i > mid)while (j <= ub)ret[k++ - lb] = arr[j++];
+	else while (i <= mid) ret[k++ - lb] = arr[i++];
+	for (int l(lb); l <= ub; l++)arr[l] = ret[l - lb];
+	delete[]ret;
+}
 
 void heapify(int* arr, int size, int node) {
 	int l(node * 2 + 1), r((node + 1) * 2), larg(node);
@@ -25,10 +39,60 @@ int partition(int* arr, int lb, int ub) {
 	return end;
 }
 
+void csort(int* arr, int size, int base) {
+	int count[10] = { 0 }, * ret(new int[size]);
+	for (int i(0); i < size; i++)count[(arr[i] / base) % 10]++;
+	for (int i(1); i < 10; i++)count[i] += count[i - 1];
+	for (int i(size - 1); i >= 0; i--)ret[--count[(arr[i] / base) % 10]] = arr[i];
+	for (int i(0); i < size; i++)arr[i] = ret[i];
+	delete[]ret;
+}
+
+void isort(vector<float>arr, int size) {
+	for (int i(1); i < size; i++) {
+		int j(i);
+		while (j > 0 && arr[j] < arr[j - 1]) {
+			swap(arr[j], arr[j - 1]);
+			j--;
+		}
+	}
+}
+
 void swap(int& x, int& y) {
 	x = x ^ y;
 	y = x ^ y;
 	x = x ^ y;
+}
+
+void busort(float* f_arr, int size) {
+	vector <vector<float>> buckets;
+	buckets.resize(size);
+	for (int i(0); i < size; i++)
+		buckets[floor(size * f_arr[i])].push_back(f_arr[i]);
+	for (int i(0); i < size; i++)
+		isort(buckets[i], buckets[i].size());
+	int index(0);
+	for (auto val1 : buckets)
+		for (auto val2 : val1)
+			f_arr[index++] = val2;
+}
+
+void rsort(int* arr, int size) {
+	int max(arr[0]);
+	for (int i(1); i < size; i++)
+		if (arr[i] > max)max = arr[i];
+	for (int base = 1; max / base > 0; base *= 10)
+		csort(arr, size, base);
+}
+
+void csort(int* arr, int size, int lb, int ub) {
+	int* count(new int[ub - lb]()), * ret(new int[size]);
+	for (int i(0); i < size; i++)count[arr[i]]++;
+	for (int i(1); i < ub - lb; i++)count[i] += count[i - 1];
+	for (int i(size - 1); i >= 0; i--)ret[--count[arr[i]]] = arr[i];
+	for (int i(0); i < size; i++)arr[i] = ret[i];
+	delete[]count;
+	delete[]ret;
 }
 
 void bsort(int* arr, int size) {
@@ -77,10 +141,20 @@ void hsort(int* arr, int size) {
 	}
 }
 
-void call_function(int call_index, int size, int* arr) {
-	char names[9][15] = { "Bubble Sort ","Insertion Sort","Selection Sort","Quick Sort","Heap Sort","Merge Sort","Counting Sort","Radix Sort","Bucket Sort" };
-	for (int i(0); i < size; i++) arr[i] = rand() % size + 0;
+void msort(int* arr, int lb, int ub) {
+	int mid((ub + lb) / 2);
+	if (lb < ub) {
+		msort(arr, lb, mid);
+		msort(arr, mid + 1, ub);
+		merge(arr, lb, mid, ub);
+	}
+}
+
+void call_function(int call_index, int size, int* arr, float* f_arr, long long* time) {
+	if (call_index == 9)
+		for (int i(0); i < size; i++)f_arr[i] = arr[i] / 1000.0;
 	auto initial(high_resolution_clock::now());
+	ios_base::sync_with_stdio(false);
 	switch (call_index)
 	{
 	case 1:bsort(arr, size);
@@ -93,18 +167,43 @@ void call_function(int call_index, int size, int* arr) {
 		break;
 	case 5:hsort(arr, size);
 		break;
+	case 6:msort(arr, 0, size - 1);
+		break;
+	case 7:csort(arr, size, 0, 1000);
+		break;
+	case 8:rsort(arr, size);
+		break;
+	case 9:
+		busort(f_arr, size);
+		break;
 	}
 	auto final(high_resolution_clock::now());
-	auto duration(duration_cast<microseconds>(final - initial).count());
-	cout << "\n\nTime Taken by " << names[call_index - 1] << ": ";
-	cout << duration << " microseconds\n";
+	time[call_index - 1] = duration_cast<microseconds>(final - initial).count();
 }
 
 int main() {
-	int size(250);
-	int* array(new int[size]);
-	for (int i(1); i <= 5; i++)call_function(i, size, array);
-	cout << "\n\nThe Sorted Array is : \n\n";
+	int size(250), range_l(0), range_u(1000), print_width[9] = { 10,8,8,12,13,12,9,12,11 };
+	char names[9][15] = { "Bubble Sort ","Insertion Sort","Selection Sort","Quick Sort","Heap Sort","Merge Sort","Counting Sort","Radix Sort","Bucket Sort" };
+	cout << "Size of the array being sorted " << size;
+	cout << "\nEach element ranging between " << range_l << " to " << range_u << endl;
+	int* array(new int[size]), * backup(new int[size]);
+	long long* time(new long long[9]());
+	float* f_array(new float[size]());
+	for (int i(0); i < size; i++) backup[i] = rand() % range_u + range_l;
+	for (int i(1); i <= 9; i++) { 
+		if(!(i==9))for (int j(0); j < size; j++)array[j] = backup[j];
+		call_function(i, size, array, f_array, time);
+	}
+	cout << "\nThe Sorted Array is : \n\n";
 	for (int i(0); i < size; i++)cout << array[i] << ' ';
-	cout << endl;
+	cout << "\n\nThe Sorted decimal values array is : \n\n";
+	for (int i(0); i < size; i++)cout << f_array[i] << ' ';
+	cout << "\n\nTime Data : \n";
+	
+	for (int i(0); i < 9; i++) {
+		cout << endl << i + 1 << "-Time Taken by " << names[i] << setw(print_width[i]) << ":   ";
+		cout << time[i];
+		cout << " microseconds";
+	}
+	cout << endl << endl;
 }
